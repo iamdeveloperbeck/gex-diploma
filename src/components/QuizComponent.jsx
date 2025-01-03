d29bc329ace5ed58c6c4898e7ec3156bac11b3ef
Developer Beck <85181067+iamdeveloperbeck@users.noreply.github.com>
2025-01-03 13:14:40 +0500

    Update QuizComponent.jsx

--- src/components/QuizComponent.jsx
+++ src/components/QuizComponent.jsx
@@ -47,7 +47,7 @@ export default function QuizComponent() {
       const interval = setInterval(() => {
         setTimer((prev) => prev - 1);
       }, 1000);
-      clearInterval(interval);
+      return () => clearInterval(interval);
     } 
     if (timer === 0) {
       handleNext();
